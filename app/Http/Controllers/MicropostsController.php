e3fc17219927e4988f615afe82734354a0418463
canon <ohshu.masamune@gmail.com>
2019-10-02 13:13:48 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -9,10 +9,10 @@ class MicropostsController extends Controller
     public function index()
     {
         $data = [];
-        if (\Auth::check()){
+        if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-            
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
